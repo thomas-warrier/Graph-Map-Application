6b3a76d9cb14eb88e0a61f05cd16ed9e422c11f4
MENARD-BEDIANT VIRGILE p2100284 <virgile.menard-bediant@etu.univ-lyon1.fr>
2022-03-25 16:55:22 +0100

    Graph & Lien

--- src/grama/graph/Graph.java
+++ src/grama/graph/Graph.java
@@ -1,4 +1,26 @@
 package grama.graph;
-public class Graph {
 
+import java.util.LinkedList;
+import java.util.List;
+
+public class Graph {
+    private final List<Noeud> noeuds;
+
+    Graph() {
+        noeuds = new LinkedList<>();
+    }
+
+    public void addNoeud(Noeud noeud) {
+        if (!noeuds.contains(noeud)) {
+            noeuds.add(noeud);
+        }
+    }
+
+    public void clear(){
+        noeuds.clear();
+    }
+
+    public boolean noeudExist(Noeud noeud){
+        return noeuds.contains(noeud);
+    }
 }
--- src/grama/graph/Lien.java
+++ src/grama/graph/Lien.java
@@ -1,4 +1,26 @@
 package grama.graph;
 public class Lien {
+    private char typeLien; //A : autoroute ; N : national ; D : départemental
+    private final int kilometrage;
 
+    private final Noeud destination;
+
+    public Lien(char typeLien, int kilometrage, Noeud destination) {
+        this.typeLien = typeLien;
+        this.kilometrage = kilometrage;
+        this.destination = destination;
+    }
+
+    public void setTypeLien(char typeLien) {
+        if(typeLien == 'A' || typeLien == 'N' || typeLien == 'D')
+            this.typeLien = typeLien;
+    }
+
+    public char getTypeLien() {
+        return typeLien;
+    }
+
+    public int getKilometrage() {
+        return kilometrage;
+    }
 }
