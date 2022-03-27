52473f9f4d263e0f0dde3361498904078d708964
MENARD-BEDIANT VIRGILE p2100284 <virgile.menard-bediant@etu.univ-lyon1.fr>
2022-03-27 10:01:00 +0200

    avencement lecture fichier

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
-import java.io.EOFException;
+import grama.graph.Lien;
+import grama.graph.Noeud;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -11,17 +17,50 @@ public class Main {
         try {
             FileInputStream stream = new FileInputStream(file);
 
-            try {
-                String line;
-                while ((line = readUntil(stream, '\n')) != null) {
-                    System.out.println(line);
+
+            String line;
+            int i = 0;
+            Pattern namePattern = Pattern.compile("^[^:]*");
+
+            while ((line = readUntil(stream, ";;")) != null) {
+                line = line.replaceAll("[\n\t\r]", ""); // remove posible '\n' or '\t' or '\r'
+
+                String name = null;
+                Matcher nameMatch = namePattern.matcher(line);
+                if (nameMatch.find()) {
+                    name = nameMatch.group();
+                    line = line.substring(nameMatch.group().length() + 1); // remove the name from the reste of the String
                 }
-            } catch (EOFException eof) {
-                System.out.println("end boucle");
+                String[] coupleLienNeoud = line.split(";");
+                List<Lien> liens = new ArrayList<>();
+                for (String couple : coupleLienNeoud){
+                    String[] both = couple.split("::");
+
+                    both[0] = both[0].replaceAll(" ", "");
+                    char type = both[0].charAt(0);
+                    int distance = Integer.valueOf(both[0].substring(2));
+
+                    both[1] = both[1].replaceAll(" ", "");
+                    char typeDst = both[1].charAt(0);
+                    String nameDst = both[1].substring(2);
+
+                    Noeud node = new Noeud(typeDst, nameDst);
+                    Lien lien = new Lien(type, distance, node);
+
+                    System.out.println(lien);
+                    liens.add(lien);
+                }
+
+
+
+//                System.out.println(both[0] + "=" + both[1].split(";"));
             }
+
             stream.close();
         } catch (IOException e) {
-            System.out.println(e.getMessage());
+            System.out.println("Error : " + e.getMessage());
+        } catch (Exception ex) {
+            System.out.println("Error " + ex.getMessage());
         }
     }
 
@@ -32,6 +71,27 @@ public class Main {
             str += (char) unsignedByte;
         }
         if (unsignedByte < 0 && str.isEmpty()) {
+            return null;
+        }
+        return str;
+    }
+
+    public static String readUntil(FileInputStream stream, String endStr) throws IOException {
+        int unsignedByte = -1;
+        String str = "";
+        int countEnd = 0;
+        int size = endStr.length();
+        while (countEnd < size && (unsignedByte = stream.read()) > -1) {
+            if ((char) unsignedByte == endStr.charAt(countEnd)) {
+                countEnd++;
+            } else {
+                countEnd = 0;
+            }
+            str += (char) unsignedByte;
+        }
+        if (countEnd == size) {
+            str = str.substring(0, str.length() - 2);
+        } else if (unsignedByte < 0) {
             return null;
         }
         return str;
