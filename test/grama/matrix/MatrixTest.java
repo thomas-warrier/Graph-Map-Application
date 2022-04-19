03e1388e422f878a8c50ef17b0d08bb1ca735018
MENARD-BEDIANT VIRGILE p2100284 <virgile.menard-bediant@etu.univ-lyon1.fr>
2022-04-19 22:31:46 +0200

    test class for Matrix

--- test/grama/matrix/MatrixTest.java
+++ test/grama/matrix/MatrixTest.java
@@ -5,7 +5,6 @@
 package grama.matrix;
 
 import grama.calcule.matrix.Matrix;
-import grama.calcule.num.Num;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -41,44 +40,23 @@ public class MatrixTest {
     @Test
     public void testEquals() {
         System.out.println("equals");
-        Num[][] arr = {
-            {new Num(1), new Num(2), new Num(3)},
-            {new Num(4), new Num(5), new Num(6)},
-            {new Num(7), new Num(8), new Num(9)}
+        Integer[][] arr = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
         };
         Matrix instance = new Matrix(arr);
         Matrix instance2 = new Matrix(arr);
         assertEquals(instance, instance2);
 
-        arr[1][1] = new Num(1);
+        arr[1][1] = 1;
         instance2 = new Matrix(arr);
 
         assertNotEquals(instance, instance2);
-        
-        assertNotEquals(instance2, new Matrix<Num>(3, new Num(0)));
-        
-        assertEquals(new Matrix<Num>(5, new Num(7)), new Matrix<Num>(5, new Num(7)));
-    }
 
-    @Test
-    public void testAdd() {
-        System.out.println("add");
-        Num[][] arr = {
-            {new Num(1), new Num(2), new Num(3)},
-            {new Num(4), new Num(5), new Num(6)},
-            {new Num(7), new Num(8), new Num(9)}
-        };
-        Matrix instance = new Matrix(arr);
-        Matrix doble = instance.add(instance);
-        Num[][] arr2 = {
-            {new Num(2), new Num(4), new Num(6)},
-            {new Num(8), new Num(10), new Num(12)},
-            {new Num(14), new Num(16), new Num(18)}
-        };
-        Matrix m = new Matrix<>(arr2);
-        System.out.println(doble);
-        System.out.println(m);
-        assertEquals(doble, m);
+        assertNotEquals(instance2, new Matrix<Integer>(3, 0));
+
+        assertEquals(new Matrix<Integer>(5, 7), new Matrix<Integer>(5, 7));
     }
 
     /**
@@ -87,18 +65,18 @@ public class MatrixTest {
     @Test
     public void testToString() {
         System.out.println("toString");
-        Num[][] arr = {
-            {new Num(1), new Num(2), new Num(3)},
-            {new Num(4), new Num(5), new Num(6)},
-            {new Num(7), new Num(8), new Num(9)}
+        Integer[][] arr = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
         };
         Matrix instance = new Matrix(arr);
-        String expResult = "Matrix{matrix=[[1, 2, 3], [4, 5, 6], [7, 8, 9]]}";
+        String expResult = "[1, 2, 3]\n[4, 5, 6]\n[7, 8, 9]\n";
         String result = instance.toString();
         assertEquals(expResult, result);
 
-        instance = new Matrix(4, new Num(0));
-        expResult = "Matrix{matrix=[[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]}";
+        instance = new Matrix(4, 0);
+        expResult = "[0, 0, 0, 0]\n[0, 0, 0, 0]\n[0, 0, 0, 0]\n[0, 0, 0, 0]\n";
         result = instance.toString();
         assertEquals(expResult, result);
     }
