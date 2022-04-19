e7e8fcf013026cb4cdc4b396660c8dfbf0922cf9
MENARD-BEDIANT VIRGILE p2100284 <virgile.menard-bediant@etu.univ-lyon1.fr>
2022-04-19 17:36:38 +0200

    Simple operator servais a rien

--- src/grama/calcule/matrix/Matrix.java
+++ src/grama/calcule/matrix/Matrix.java
@@ -3,21 +3,15 @@ package grama.calcule.matrix;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import grama.calcule.models.SimpleOperator;
-import grama.calcule.num.Num;
 
 /**
  *
  * @author wwwazz
  * @param <T>
  */
-public class Matrix<T extends SimpleOperator<T>> implements SimpleOperator<Matrix<T>>{
+public class Matrix<T> {
 
     private List<List<T>> matrix;
-
-    public Matrix() {
-        matrix = new ArrayList<List<T>>();
-    }
 
     public Matrix(int length, T defaut) {
         matrix = new ArrayList<List<T>>();
@@ -50,40 +44,6 @@ public class Matrix<T extends SimpleOperator<T>> implements SimpleOperator<Matri
     }
 
     @Override
-    public Matrix<T> add(Matrix<T> other) {
-        Matrix<T> sum = new Matrix<>(this.matrix.size(), null);
-        for (int i = 0; i < matrix.size(); i++) {
-            List<T> line = new ArrayList<>();
-
-            for (int j = 0; j < matrix.get(i).size(); j++) {
-                line.add(matrix.get(i).get(j).add(other.matrix.get(i).get(j)));
-            }
-            sum.matrix.set(i, line);
-        }
-        return sum;
-    }
-
-    @Override
-    public Matrix<T> sub(Matrix<T> other) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public Matrix<T> mul(Matrix<T> other) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-    public Matrix<T> mul(Num n){
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public Matrix<T> div(Matrix<T> other) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    
-
-    @Override
     public int hashCode() {
         int hash = 3;
         hash = 89 * hash + Objects.hashCode(this.matrix);
@@ -103,8 +63,7 @@ public class Matrix<T extends SimpleOperator<T>> implements SimpleOperator<Matri
         }
         final Matrix<?> other = (Matrix<?>) obj;
         return this.matrix.equals(other.matrix);
-    }    
-    
+    }
 
     @Override
     public String toString() {
