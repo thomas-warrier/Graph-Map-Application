31833458ca1b0a5ed36af29ff61669883e2c24ed
wwwazz <wwwazz333@gmail.com>
2022-04-20 18:07:52 +0200

    type n'est plus un char mais un enum

--- src/grama/graph/Noeud.java
+++ src/grama/graph/Noeud.java
@@ -8,29 +8,66 @@ import java.util.List;
 import java.util.Objects;
 
 public class Noeud {
-
+    
+    public enum Type {
+        VILLE('V'),
+        LOISIR('L'),
+        RESTAURANT('R'),
+        ALL('+'),
+        NONE('-');
+        
+        private final char representativeChar;
+        
+        Type(char c) {
+            this.representativeChar = c;
+        }
+        
+        public char getRepresentativeChar() {
+            return representativeChar;
+        }
+        
+        public boolean isType(Type t) {
+            return this == t || t == Type.ALL;
+        }
+        
+        public static Type getType(char c) {
+            for (Type t : Type.values()) {
+                if (t.representativeChar == c) {
+                    return t;
+                }
+            }
+            return Type.NONE;
+        }
+        
+        @Override
+        public String toString() {
+            return Character.toString(representativeChar);
+        }
+        
+    }
+    
     private final List<Lien> listLien;
-    private char typeLieu;  // V : ville , L : loisir , R : restaurant
+    private Type typeLieu;  // V : ville , L : loisir , R : restaurant
     private final String nom;
-
-    public Noeud(char newTypeLieu, String newNom) {
+    
+    public Noeud(Type newTypeLieu, String newNom) {
         listLien = new LinkedList<>();
         this.typeLieu = newTypeLieu;
         this.nom = newNom;
     }
-
-    public void setTypeLieu(char typeLieu) {
-        if (typeLieu == 'V' || typeLieu == 'L' || typeLieu == 'R') {
+    
+    public void setTypeLieu(Type typeLieu) {
+        if (typeLieu != Type.NONE && typeLieu != Type.ALL) {
             this.typeLieu = typeLieu;
         } else {
             throw new MauvaisTypeException();
         }
     }
-
-    public char getTypeLieu() {
+    
+    public Type getTypeLieu() {
         return typeLieu;
     }
-
+    
     public List<Lien> getListLien() {
         return listLien;
     }
@@ -41,7 +78,7 @@ public class Noeud {
      * @param typeVoisins le type des voisins qui doivent être récuperer
      * @return Liste des voisins direct du type typeVoisins
      */
-    public List<Noeud> getVoisinsOfType(char typeVoisins) {
+    public List<Noeud> getVoisinsOfType(Type typeVoisins) {
         List<Noeud> voisins = new ArrayList<>();
         for (Lien lien : getListLien()) {
             if (lien.getDstADepartDe(this).getTypeLieu() == typeVoisins) {
@@ -50,14 +87,14 @@ public class Noeud {
         }
         return voisins;
     }
-
+    
     public void addLien(Lien lien) {
         if (!listLien.contains(lien)) {
             listLien.add(lien);
             lien.getDstADepartDe(this).addLien(lien);//doit aussi être ajouté dans l'autre sense
         }
     }
-
+    
     public Integer getDistanceTo(Noeud node) {
         for (Lien lien : listLien) {
             if (lien.getDstADepartDe(this).equals(node)) {
@@ -66,7 +103,7 @@ public class Noeud {
         }
         return null;
     }
-
+    
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -78,12 +115,12 @@ public class Noeud {
         Noeud noeud = (Noeud) o;
         return typeLieu == noeud.typeLieu && Objects.equals(nom, noeud.nom);
     }
-
+    
     @Override
     public int hashCode() {
         return Objects.hash(typeLieu, nom);
     }
-
+    
     @Override
     public String toString() {
         return typeLieu + ":" + nom;
